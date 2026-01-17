0a3a4e2603aae20f0620186db2e0139bac0e8e9a
Luciano Cipolla Delcantare <143667181+LucianoCipolla@users.noreply.github.com>
2026-01-17 20:22:56 -0300

    primer ejercicio que hago solo y completo mezclando html,css y y js, BRASIL!!!!

---
+++ favoriteIconToggler/script.js
@@ -0,0 +1,10 @@
+const btns = document.querySelectorAll('.favorite-icon');
+
+btns.forEach(btn => btn.addEventListener('click', () => {
+    if (btn.classList.contains('filled')) {
+        btn.classList.remove('filled');
+        btn.innerHTML = '&#9825'} else {
+            btn.classList.add('filled');
+            btn.innerHTML = '&#10084'
+        }
+}));
